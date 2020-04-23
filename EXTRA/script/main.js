47c7e4446f28be7530176197d657985c5d5e4e7f
Andrea De Martino <ademartino1992@gmail.com>
2020-04-23 14:41:20 +0200

    Create: Add extra folder and create first step of exercise

---
+++ EXTRA/script/main.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+
+
+  // Descrizione:
+  // Creare un oggetto che descriva uno studente
+  // lo studente avrà  le seguenti proprietà: nome, cognome e età.
+  // Stampare attraverso il for..in tutte le proprietà (chiavi e valori).
+
+
+  
+  // Nome della repo per consegnare l’esercizio: js-oggetti-studenti
+  // OPZIONALE (in una cartella a parte dal codice per l’esercizio degli oggetti)
+  // Riprodurre la piccola chat fatta insieme durante la lezione utilizzando handlerbars.
+  // Ricordo il link alla documentazione per download/cdn e referenza sull’utilizzo base:
+  // https://handlebarsjs.com/installation/
+  // Buon lavoro ragazzi e benvenuti nel mondo degli oggetti JS e di handlebars! :top:
+
+
+
+
+
+
+
+
+
+
+  
+  
+
+});
--- script/main.js
+++ script/main.js
@@ -1,17 +1,18 @@
 $(document).ready(function () {
+  // Descrizione:
+  // Creare un oggetto che descriva uno studente
+  // lo studente avrà le seguenti proprietà: nome, cognome e età.
+  // Stampare attraverso il for..in tutte le proprietà (chiavi e valori).
 
+  var student = {
+    nome: 'Andrea',
+    cognome: 'De Martino',
+    eta: 27
+  }
 
-
-
-
-
-
-
-
-
-
-
-  
-  
+  // Print keys and values of the object "student"
+  for (var key in student){
+    console.log(key + ': ' + student[key]);
+  }
 
 });
