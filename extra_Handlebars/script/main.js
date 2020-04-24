a2be283f386a05fa22ff575d111060a0c5c7be5e
Andrea De Martino <ademartino1992@gmail.com>
2020-04-24 15:58:37 +0200

    Create: Messages at beginning + scroll fix

--- extra_Handlebars/script/main.js
+++ extra_Handlebars/script/main.js
@@ -1,15 +1,29 @@
 $(document).ready(function () {
 
+  /****************************************************
+  * TASK DAY 1
+  ****************************************************/
+
   // OPZIONALE (in una cartella a parte dal codice per l’esercizio degli oggetti)
   // Riprodurre la piccola chat fatta insieme durante la lezione utilizzando handlerbars.
   // Ricordo il link alla documentazione per download/cdn e referenza sull’utilizzo base:
   // https://handlebarsjs.com/installation/
   // Buon lavoro ragazzi e benvenuti nel mondo degli oggetti JS e di handlebars! :top:
 
+  /****************************************************
+  * TASK DAY 2
+  ****************************************************/
+
+  // Estendere la chat fatta con handlebars inserendo i messaggi iniziali, visibili al caricamento della pagina, generati da un array di oggetti.
+  // Gli oggetti conterranno proprietà come testo e orario
+  // Usiamo Handlebars per la generazione anche di questi messaggi, come avete fatto ieri per i messaggi inseriti dall’utente
+
   var sendButton = $('.send-button');
   var chatBox = $('.chat-main');
 
-
+  // Creation of the messages at the beginning (TASK 2)
+  startMessages();
+  
   // Insert message on button click
   sendButton.click(function (){
     addMessage('sent');
@@ -29,6 +43,41 @@ $(document).ready(function () {
   * FUNCTIONS
   ****************************************************/
 
+  // FUNCTION: Start messages on chat-main (TASK DAY 2)
+  function startMessages(){
+    // Handlebars Config
+    var source = $("#message-template").html();
+    var template = Handlebars.compile(source);
+
+    // Creation of array of objects "message"
+    var messages = [
+      {
+        text: 'Ciao',
+        time: '10:40',
+        type: 'sent'
+      },
+      {
+        text: 'Che si dice?',
+        time: '20:10',
+        type: 'received'
+      },
+      {
+        text: 'Tutto bene',
+        time: '22:37',
+        type: 'sent'
+      }
+    ];
+
+    for (var i = 0; i < messages.length; i++){
+      // Get html template with dinamic values
+      var html = template(messages[i]);
+      // Add html template on chat container
+      chatBox.append(html);
+      scrollChat();
+    }
+  }
+
+  // FUNCTION: Add User and Bot Messages
   function addMessage(type){
     var messageText = $('#insert-message').val().trim();
 
@@ -55,8 +104,17 @@ $(document).ready(function () {
 
     // Add html template on chat container and clean inputbox
     chatBox.append(html);
+    scrollChat()
   }
 
+  // FUNCTION: Chat scroll
+  function scrollChat(){
+    // Scroll on inserting a new message:
+    // 1) Get the height of the message chat container
+    var pixelScroll = $('.chat-main').height();
+    // 2) Scroll on the container that has the overflow-Y
+    $('.chat-main').scrollTop(pixelScroll);
+  }
 
   // FUNCTION: Get and print Time
   function timeStamp(){
@@ -66,7 +124,6 @@ $(document).ready(function () {
     var finalDate = hour + ':' + min;
     return finalDate;
   }
-
 
   // FUNCTION: Fix Time with 0 on single numbers
   function timeFix(date){
